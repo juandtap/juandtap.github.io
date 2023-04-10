6ae3712ce07b226c69e8fe00e78b47277a51b87f
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-10 00:48:43 -0500

    agregado audio a la pagina por JS

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-console.log('Hello!');
+// window.onYouTubeIframeAPIReady = function() {
+//     onYouTubeIframeAPIReady();
+// };
+// Carga la API de YouTube
+var tag = document.createElement("script");
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName("script")[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  // Crea un reproductor de YouTube con solo audio
+  player = new YT.Player("player", {
+    height: "0",
+    width: "0",
+    videoId: "wj9T0kMl2GM",
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      disablekb: 1,
+      enablejsapi: 1,
+      iv_load_policy: 3,
+      loop: 1,
+      modestbranding: 1,
+      playsinline: 1
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  // Oculta el video y reproduce solo el audio
+  player.setPlaybackQuality("small");
+  player.mute();
+  player.playVideo();
+}
