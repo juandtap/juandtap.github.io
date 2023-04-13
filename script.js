42b728923487d788f08e5b2e6fb5bd2fd7e52a06
Diego Tapia <jtapiav4@est.ups.edu.ec>
2023-04-13 08:05:30 -0500

    cambios JS

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-// window.onYouTubeIframeAPIReady = function() {
-//     onYouTubeIframeAPIReady();
-// };
-// Carga la API de YouTube
-var tag = document.createElement("script");
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName("script")[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+console.log("HELLO!")
 
-var player;
-function onYouTubeIframeAPIReady() {
-  // Crea un reproductor de YouTube con solo audio
-  player = new YT.Player("player", {
-    height: "0",
-    width: "0",
-    videoId: "wj9T0kMl2GM",
-    playerVars: {
-      autoplay: 1,
-      controls: 0,
-      disablekb: 1,
-      enablejsapi: 1,
-      iv_load_policy: 3,
-      loop: 1,
-      modestbranding: 1,
-      playsinline: 1
-    },
-    events: {
-      onReady: onPlayerReady
-    }
-  });
-}
+var element = document.getElementById('titulo1');
+element.addEventListener('click', ()=>
+  alert('clik en el titulo')
+)
 
-function onPlayerReady(event) {
-  // Oculta el video y reproduce solo el audio
-  player.setPlaybackQuality("small");
-  player.mute();
-  player.playVideo();
+element.addEventListener('mouseover',function(){
+  
+  i = i + 1;
+  console.log(i);
+  element.innerHTML = i
 }
+)
+
+var listas = document.getElementsByTagName('li')
+console.log(listas)
